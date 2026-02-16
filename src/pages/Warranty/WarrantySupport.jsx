aafeaf8b8cad878deb6b4f4dac28aedb571fda7c
Ully <alighosn2099@gmail.com>
2026-02-15 23:09:37 -0500

    scroll to top on warranty land

--- src/pages/Warranty/WarrantySupport.jsx
+++ src/pages/Warranty/WarrantySupport.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './WarrantySupport.css';
 
 export default function WarrantySupport() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="warranty-page">
       <div className="warranty-container">
